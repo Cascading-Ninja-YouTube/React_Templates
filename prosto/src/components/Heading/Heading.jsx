85cf6c91d0bc0b700f3be4ea1d8bc042a4d7e1f5
Cascading Ninja <subhajitmaji@yandex.com>
2021-05-17 23:34:39 +0530

    Source code for Prosto template

---
+++ prosto/src/components/Heading/Heading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './heading.css'
+
+function Heading(props) {
+    return (
+        <div className="heading">
+            <h6>{props.genre} {props.blogtag}</h6>
+           
+            <h2>{props.title}</h2>
+        </div>
+    )
+}
+
+export default Heading
